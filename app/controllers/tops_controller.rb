6b1710478f98d07a0a58f1c17dd320809dcd7b6a
teshibayuzu <2111140026r@ed.fuk.kindai.ac.jp>
2023-10-27 04:49:59 +0000

    top

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,10 +1,27 @@
 class TopsController < ApplicationController
   def main
+     if session[:login_uid] !=nil
+            render "main"
+        else
+            render "login"
+        end
   end
 
   def login
+     logger.debug params[:uid]
+            if User.find_by(uid: params[:uid]) and User.find_by(pass: params[:pass])
+                #params[:uid]=="kindai" and params[:pass]=="sanriko"
+                session[:login_uid]=params[:uid]
+                redirect_to top_main_path
+            else
+                render "error"
+                #logger.debug "error"
+                #render plain:"エラーです"
+            end
   end
 
   def logout
+     session.delete(:login_uid)
+            redirect_to top_main_path
   end
 end
