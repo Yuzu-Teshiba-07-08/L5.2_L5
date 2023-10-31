34f2234c8792e374d3b8590c347ac5eeed085dbc
teshibayuzu <2111140026r@ed.fuk.kindai.ac.jp>
2023-10-31 07:58:44 +0000

    likes&logout

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,13 @@ class LikesController < ApplicationController
     tweet = Tweet.find(params[:tweet_id])
     user = User.find_by(uid: session[:login_uid])
     user.like_tweets << tweet
-    redirect_to root_path
+    redirect_to tweets_path
   end
 
   def destory
     tweet = Tweet.find(params[:id])
     user = User.find_by(uid: session[:login_uid])
     tweet.likes.find_by(user_id: user.id).destroy
-    redirect_to root_path
+    redirect_to tweets_path
   end
 end
